1c562f326faaf1b54b22aec62c7b49e7304a023f
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-25 12:05:34 +0900

    refactoring

--- bench/bench_aes_ctr.cpp
+++ bench/bench_aes_ctr.cpp
@@ -6,73 +6,7 @@ using namespace std;
 using namespace clt;
 using namespace clt::bench;
 
-template <class T, class U, class V>
-inline void do_enc(T &out, const U &in, const V &cipher)
-{
-    assert((out.size() % aes128::block_bytes) == 0);
-    const size_t num_bytes = out.size();
-    const size_t num_blocks = num_bytes / aes128::block_bytes;
-    const auto elapsed_time =
-        measure([&]() { cipher.enc(out.data(), in.data(), num_blocks); });
-    const auto bytes_per_sec = num_bytes / elapsed_time;
-    const auto blocks_per_sec = num_blocks / elapsed_time;
-    fmt::print("aes128enc,{},{:.5e},{:.5e}\n", num_bytes, bytes_per_sec,
-               blocks_per_sec);
-}
-
-template <class T, class U, class V>
-inline void do_dec(T &out, const U &in, const V &cipher)
-{
-    assert((out.size() % clt::aes128::block_bytes) == 0);
-    const size_t num_bytes = out.size();
-    const size_t num_blocks = num_bytes / clt::aes128::block_bytes;
-    const double elapsed_time =
-        measure([&]() { cipher.dec(out.data(), in.data(), num_blocks); });
-    const auto bytes_per_sec = num_bytes / elapsed_time;
-    const auto blocks_per_sec = num_blocks / elapsed_time;
-    fmt::print("aes128dec,{},{:.5e},{:.5e}\n", num_bytes, bytes_per_sec,
-               blocks_per_sec);
-}
-
-template <class T>
-inline bool eq_check(const vector<T> &buff0, const vector<T> &buff1)
-{
-    assert(buff0.size() == buff1.size());
-    const size_t num_elems = buff0.size();
-    for (size_t i = 0; i < num_elems; i++) {
-        if (buff0[i] != buff1[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-template <class T> inline void do_enc_dec_iteration(const T &cipher)
-{
-    size_t current = start_byte_size;
-    vector<uint8_t> buff, enc_buff;
-    buff.reserve(stop_byte_size);
-    enc_buff.reserve(stop_byte_size);
-#ifndef NDEBUG
-    fmt::print(cerr, "Decryption check is enabled.\n");
-    vector<uint8_t> dec_buff;
-    dec_buff.reserve(stop_byte_size);
-#endif
-    while (current <= stop_byte_size) {
-        buff.resize(current);
-        enc_buff.resize(buff.size());
-        init(buff, current);
-        do_enc(enc_buff, buff, cipher);
-#ifndef NDEBUG
-        dec_buff.resize(buff.size());
-        do_dec(dec_buff, enc_buff, cipher);
-        assert(eq_check(buff, dec_buff));
-#endif
-        current <<= 1;
-    }
-}
-
-template <class T, class U> void do_aesctr(T &out, const U &cipher)
+template <class T, class U> void do_aes_ctr(T &out, const U &cipher)
 {
     assert((out.size() % clt::aes128::block_bytes) == 0);
     const size_t num_bytes = out.size();
@@ -85,7 +19,7 @@ template <class T, class U> void do_aesctr(T &out, const U &cipher)
                blocks_per_sec);
 }
 
-template <class T> void do_aesctr_iteration(const T &cipher)
+template <class T> void do_aes_ctr_iteration(const T &cipher)
 {
     constexpr size_t num_loop = 1;
     size_t current = start_byte_size;
@@ -94,7 +28,7 @@ template <class T> void do_aesctr_iteration(const T &cipher)
     while (current <= stop_byte_size) {
         buff.resize(current);
         for (size_t i = 0; i < num_loop; i++) {
-            do_aesctr(buff, cipher);
+            do_aes_ctr(buff, cipher);
         }
         current <<= 1;
     }
@@ -105,6 +39,6 @@ int main()
     AES128::key_t key;
     gen_key(key);
     AES128 cipher(key);
-    do_enc_dec_iteration(cipher);
+    do_aes_ctr_iteration(cipher);
     return 0;
 }
