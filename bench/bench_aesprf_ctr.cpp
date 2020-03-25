b1b5498f1887277130b846992bdf9ab38783b06c
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-25 12:47:59 +0900

    refactoring

--- bench/bench_aesprf_ctr.cpp
+++ bench/bench_aesprf_ctr.cpp
@@ -6,28 +6,31 @@ using namespace std;
 using namespace clt;
 using namespace clt::bench;
 
-template <class T, class U> inline void do_aesprf_ctr(T &out, const U &cipher)
+template <class T, class U> inline void do_aesprf_ctr(T &out, const U &prf)
 {
     assert((out.size() % clt::aes128::block_bytes) == 0);
     const size_t num_bytes = out.size();
     const size_t num_blocks = num_bytes / clt::aes128::block_bytes;
     const double elapsed_time =
-        measure([&]() { cipher.ctr_stream(out.data(), num_blocks, 0); });
+        measure([&]() { prf.ctr_stream(out.data(), num_blocks, 0); });
     const auto bytes_per_sec = num_bytes / elapsed_time;
     const auto blocks_per_sec = num_blocks / elapsed_time;
-    fmt::print("aesprf128_ctr,{},{:.5e},{:.5e}\n", num_bytes, bytes_per_sec,
+    fmt::print("aes128prf_ctr,{},{:.5e},{:.5e}\n", num_bytes, bytes_per_sec,
                blocks_per_sec);
 }
 
-template <class T> inline void do_aesprf_ctr_iteration(const T &cipher)
+template <class T> inline void do_aesprf_ctr_iteration(const T &prf)
 {
+    constexpr size_t num_loop = 1;
     size_t current = start_byte_size;
     vector<uint8_t> buff;
     buff.reserve(stop_byte_size);
     fmt::print("mode,bytes,bytes/s,blocks/s\n");
     while (current <= stop_byte_size) {
         buff.resize(current);
-        do_aesprf_ctr(buff, cipher);
+        for (size_t i = 0; i < num_loop; i++) {
+            do_aesprf_ctr(buff, prf);
+        }
         current <<= 1;
     }
 }
@@ -38,7 +41,7 @@ int main()
     AES128::key_t key;
     gen_key(key);
     fmt::print(cerr, "key = {}\n", clt::join(key));
-    AES128 cipher(key);
-    do_aesprf_ctr_iteration(cipher);
+    AESPRF128 prf(key.data());
+    do_aesprf_ctr_iteration(prf);
     return 0;
 }
---
+++ bench/run_benchmarks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "Current directory = $(pwd)"
+bench_marks=$(ls -1 bench_*)
+echo "Current benchmarks:"
+echo "${bench_marks}"
+echo "Running..."
+for bench in ${bench_marks}; do
+    echo "Execute: ${bench}"
+    ./${bench}
+done
