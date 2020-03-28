c2f55156b6016f287f5bc95ed564acc4d7e5bad8
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-29 01:09:06 +0900

    refactoring

--- search_module.cmake
+++ search_module.cmake
@@ -4,8 +4,6 @@ if(OpenMP_FOUND)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
 endif()
 
-find_package(GTest REQUIRED)
-
 if(NOT (IS_DIRECTORY "${CMAKE_SOURCE_DIR}/third_party"))
   make_directory("${CMAKE_SOURCE_DIR}/third_party")
 endif()
