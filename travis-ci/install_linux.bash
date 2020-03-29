78039ead03a0a72110358984c8bd2a93bb65299c
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-29 16:36:51 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -2,4 +2,4 @@
 set -ev
 docker exec -ti $MY_NAME bash -c "apt update" > /dev/null
 docker exec -ti $MY_NAME bash -c "apt upgrade -y" > /dev/null
-docker exec -ti $MY_NAME bash -c "apt install -y build-essential cmake libfmt-dev"
+docker exec -ti $MY_NAME bash -c "apt install -y build-essential cmake libfmt-dev libgtest-dev"
