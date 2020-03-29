683b277cb600bb98670c6249f94b01e3694bc330
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-29 17:06:06 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -3,7 +3,10 @@ set -ev
 alias docw="docker exec -ti ${MY_NAME} bash -c"
 docker exec -ti ${MY_NAME} bash -c "apt update" > /dev/null
 docker exec -ti ${MY_NAME} bash -c "apt upgrade -y" > /dev/null
-docker exec -ti ${MY_NAME} bash -c "apt install -y build-essential git cmake libfmt-dev libgtest-dev"
-docker exec -ti ${MY_NAME} bash -c "pwd"
+docker exec -ti ${MY_NAME} bash -c "apt install -y build-essential git cmake libfmt-dev libgtest-dev" > /dev/null
+
+docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/fmtlib/fmt.git"
+docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
+
 docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/google/googletest.git"
-docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir -p build; cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j8; make install; ldconfig"
+docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir -p build; cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j; make install; ldconfig"
