dab73282fa3d9cb1507b1f3d8b041f6701382238
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-29 16:55:17 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 alias docw="docker exec -ti ${MY_NAME} bash -c"
-docw "apt update" > /dev/null
-docw "apt upgrade -y" > /dev/null
-docw "apt install -y build-essential git cmake libfmt-dev libgtest-dev"
-docw "pwd"
-docw "git clone https://github.com/google/googletest.git"
-docw "cd googletest; mkdir build && cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j8; sudo make install; sudo ldconfig"
+docker exec -ti ${MY_NAME} bash -c "apt update" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "apt upgrade -y" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "apt install -y build-essential git cmake libfmt-dev libgtest-dev"
+docker exec -ti ${MY_NAME} bash -c "pwd"
+docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/google/googletest.git"
+docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir build && cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j8; sudo make install; sudo ldconfig"
