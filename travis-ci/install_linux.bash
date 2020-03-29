dbada4c2bb2bf9fe3b5ee045d60bdc87234b372e
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-29 16:27:01 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
-docker exec -ti $MY_NAME bash -c "apt update" > /dev/null
-docker exec -ti $MY_NAME bash -c "apt install -y build-essential cmake g++-9" > /dev/null
+docker exec -ti $MY_NAME bash -c "apt update"
+docker exec -ti $MY_NAME bash -c "apt install -y build-essential cmake g++-9"
