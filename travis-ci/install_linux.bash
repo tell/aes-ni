f972f1b691957ce301eb78debe80f721515fb114
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-29 16:50:32 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ev
-docker exec -ti $MY_NAME bash -c "apt update" > /dev/null
-docker exec -ti $MY_NAME bash -c "apt upgrade -y" > /dev/null
-docker exec -ti $MY_NAME bash -c "apt install -y build-essential cmake libfmt-dev libgtest-dev"
+alias docw="docker exec -ti ${MY_NAME} bash -c"
+docw "apt update" > /dev/null
+docw "apt upgrade -y" > /dev/null
+docw "apt install -y build-essential git cmake libfmt-dev libgtest-dev"
+docw "pwd"
+docw "git clone https://github.com/google/googletest.git"
+docw "cd googletest; mkdir build && cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j8; sudo make install; sudo ldconfig"
