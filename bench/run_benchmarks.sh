3f85f95c5efe3dac640e70a72543d3e30870da08
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-03-31 15:21:54 +0900

    use find command instead of ls

--- bench/run_benchmarks.sh
+++ bench/run_benchmarks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 echo "Current directory = $(pwd)"
-bench_marks=$(ls -1 bench_*)
+bench_marks=$(find . -maxdepth 1 -iname "bench_*" -executable)
 echo "Current benchmarks:"
 echo "${bench_marks}"
 echo "Running..."
