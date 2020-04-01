c91e88c941a9eb2fbcb29de2bc1cc7a23e1c75f7
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-04-01 10:23:59 +0900

    add benchmark plotter

--- bench/run_benchmarks.sh
+++ bench/run_benchmarks.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/bash
 
 echo "Current directory = $(pwd)"
-bench_marks=$(find . -maxdepth 1 -iname "bench_*" -executable)
+bench_marks=$(find . -maxdepth 1 -iname "bench_*" -not -iname "bench_shuffle_*" -perm -100)
 echo "Current benchmarks:"
 echo "${bench_marks}"
 echo "Running..."
+
+plotfile="autogen_plotting.plot"
+rm -f ${plotfile}
+touch ${plotfile}
+
+echo "plot \\" > ${plotfile}
+
 for bench in ${bench_marks}; do
     echo "Execute: ${bench}"
-    ./${bench} > ./${bench}_result.txt
+    ${bench} > ./${bench}_result.csv
+    echo "\"${bench}_result.csv\" using (column(2)):(valid(4) ? column(4) : NaN) with linespoints title \"${bench}\" noenhanced, \\" >> ${plotfile}
 done
