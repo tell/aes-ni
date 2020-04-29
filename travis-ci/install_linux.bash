7989ba4e1834d097fe804e3086fc25a63551c76f
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-04-29 23:31:23 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -6,7 +6,8 @@ docker exec -ti ${MY_NAME} bash -c "DEBIAN_FRONTEND=noninteractive apt install -
 
 docker exec -ti ${MY_NAME} bash -c "git clone --depth 1 https://github.com/fmtlib/fmt.git ${MY_TARGET_MOUNT}/third_party/fmt"
 #docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
-#docker exec -ti ${MY_NAME} bash -c "apt install -q -y libfmt-dev libspdlog-dev"
+
+docker exec -ti ${MY_NAME} bash -c "apt install -q -y libspdlog-dev"
 
 docker exec -ti ${MY_NAME} bash -c "git clone --depth 1 https://github.com/google/googletest.git"
 docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir -p build; cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j; make install; ldconfig"
