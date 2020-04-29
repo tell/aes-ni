ccd28981beb0d86670ef40eaa0ce6bb2f4a6e343
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-04-29 23:40:19 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
-docker exec -ti ${MY_NAME} bash -c "apt update -q"
-docker exec -ti ${MY_NAME} bash -c "apt upgrade -q -y"
-docker exec -ti ${MY_NAME} bash -c "DEBIAN_FRONTEND=noninteractive apt install -q -y build-essential git cmake libgmp-dev"
+docker exec -ti ${MY_NAME} bash -c "apt update -q" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "apt upgrade -q -y" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "DEBIAN_FRONTEND=noninteractive apt install -q -y build-essential git cmake libgmp-dev" > /dev/null
 
 docker exec -ti ${MY_NAME} bash -c "git clone --depth 1 https://github.com/fmtlib/fmt.git ${MY_TARGET_MOUNT}/third_party/fmt"
 #docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
