e59c4b73e82492e871d690d656b4e3fe8f12941c
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-04-29 23:10:46 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -6,7 +6,7 @@ docker exec -ti ${MY_NAME} bash -c "apt install -q -y build-essential git cmake 
 
 #docker exec -ti ${MY_NAME} bash -c "git clone --depth 1 https://github.com/fmtlib/fmt.git"
 #docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
-docker exec -ti ${MY_NAME} bash -c "apt install -q -y libfmt-dev"
+docker exec -ti ${MY_NAME} bash -c "apt install -q -y libfmt-dev libspdlog-dev"
 
 docker exec -ti ${MY_NAME} bash -c "git clone --depth 1 https://github.com/google/googletest.git"
 docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir -p build; cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j; make install; ldconfig"
