0ce1e125a97eaccddc6b6b5de5f92d0a1d0309f0
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-04-30 01:02:58 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -7,7 +7,7 @@ docker exec -ti ${MY_NAME} bash -c "DEBIAN_FRONTEND=noninteractive apt install -
 docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/fmtlib/fmt.git -b 6.1.2 --depth 1 ${MY_TARGET_MOUNT}/third_party/fmt"
 #docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
 
-docker exec -ti ${MY_NAME} bash -c "apt install -q -y libspdlog-dev"
+docker exec -ti ${MY_NAME} bash -c "apt install -q -y libspdlog-dev libgtest-dev"
 
-docker exec -ti ${MY_NAME} bash -c "git clone --depth 1 https://github.com/google/googletest.git"
-docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir -p build; cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j; make install; ldconfig"
+#docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/google/googletest.git --depth 1"
+#docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir -p build; cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j; make install; ldconfig"
