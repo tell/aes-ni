b53d351f13e1d744445caa548f03666246079321
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-04-30 00:01:52 +0900

    update

--- travis-ci/script_linux.bash
+++ travis-ci/script_linux.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
-docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; mkdir -p build; cd build; cmake -DCMAKE_CXX_FLAGS=-std=c++2a -DCMAKE_BUILD_TYPE=Release ..; make"
+docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; mkdir -p build; cd build; cmake -DCMAKE_BUILD_TYPE=Release ..; make"
 docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT/build; ctest --verbose"
