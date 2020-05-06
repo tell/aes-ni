2d7671de5461ea66e9c1beee36ad0ec16da0a414
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-05-06 18:48:25 +0900

    update

--- travis-ci/before_script_linux.bash
+++ travis-ci/before_script_linux.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
-docker exec -ti $MY_NAME bash -c "echo $CC; $CC --version"
-docker exec -ti $MY_NAME bash -c "echo $CXX; $CXX --version"
+docker exec -ti $MY_NAME bash -c "$CC --version"
+docker exec -ti $MY_NAME bash -c "$CXX --version"
