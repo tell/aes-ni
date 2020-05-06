b857686a15ea73bfa06229a4f3109d73b1cf2367
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-05-06 18:41:57 +0900

    update

--- travis-ci/before_install_linux.bash
+++ travis-ci/before_install_linux.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -ev
+
+echo Use docker image $MY_TARGET_DIST
+echo   with mount point $TRAVIS_BUILD_DIR:$MY_TARGET_MOUNT
+
 docker pull $MY_TARGET_DIST
 docker run --name $MY_NAME -v $TRAVIS_BUILD_DIR:$MY_TARGET_MOUNT -td $MY_TARGET_DIST /bin/bash
