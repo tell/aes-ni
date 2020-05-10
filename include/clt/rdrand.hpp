5cb152c9eee9e4288f675eaae72faa3cf47bad04
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-05-11 00:08:25 +0900

    update

--- include/clt/rdrand.hpp
+++ include/clt/rdrand.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 
@@ -13,6 +14,7 @@ void rdrand(uint64_t *out, const size_t num_elems) noexcept;
 using generic_rdrand_t = uint64_t;
 template <class T> void rdrand(T *out, const size_t num_elems) noexcept
 {
+    assert(sizeof(T) * num_elems >= sizeof(generic_rdrand_t));
     rdrand(reinterpret_cast<uint64_t *>(out), num_elems);
 }
 } // namespace rng
