906fd3bdccd3670b937d47d4ece44eb424582f7a
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-09-04 08:11:47 +0900

    update

--- bench/bench_shuffle_fy.cpp
+++ bench/bench_shuffle_fy.cpp
@@ -16,24 +16,27 @@ using namespace clt::bench;
 inline void do_shuffle_ys_iteration()
 {
     using elem_t = uint32_t;
-    fmt::print("mode,num_32bit_elems,seconds,num_32bit_elems/seconds\n");
-    const std::string fmt_str = "shuffle_ys_aesprf128,{},{:e},{:e}\n";
     constexpr size_t step = 1;
-    constexpr size_t num_loop = 23;
-    constexpr size_t num_iteration = 10;
+    constexpr size_t num_cases = 23;
+    constexpr size_t num_samples = 10;
+    const std::string fmt_str = "shuffle_ys_aesprf128,{},{:e}\n";
+    fmt::print("mode,num_32bit_elems,seconds\n");
+    for (size_t i = 0; i < num_cases; i++) {
+        array<double, num_samples> time_samples;
+        const size_t num_elems = start_byte_size << (i * step);
 #pragma omp parallel for
-    for (size_t j = 0; j < num_iteration; j++) {
-#pragma omp parallel for
-        for (size_t i = 0; i < num_loop; i++) {
+        for (size_t j = 0; j < num_samples; j++) {
             AES128::key_t key = gen_key();
             AESPRF128_CTR prf(key.data());
-            const size_t current = start_byte_size << (i * step);
-            vector<elem_t> buff(current);
+            vector<elem_t> buff(num_elems);
             iota(begin(buff), end(buff), 0);
-            print_benchmark([&]() { shuffle(buff.data(), size(buff), prf); },
-                            fmt_str, size(buff));
+            const auto elapsed_time = measure_static(
+                [&]() { shuffle(buff.data(), size(buff), prf); });
             dummy_call(reinterpret_cast<void *>(buff.data()));
+            time_samples[j] = elapsed_time;
         }
+        const auto time_median = median(time_samples);
+        fmt::print(fmt_str, num_elems, time_median);
     }
 }
 
@@ -56,6 +59,7 @@ inline void do_shuffle_ys_iteration_10()
             vector<elem_t> buff(num_elems);
             const auto elapsed_time = measure_static(
                 [&]() { shuffle(buff.data(), size(buff), prf); });
+            dummy_call(reinterpret_cast<void *>(buff.data()));
             time_samples[j] = elapsed_time;
         }
         const auto time_median = median(time_samples);
@@ -67,7 +71,7 @@ int main()
 {
     print_diagnosis();
     print_omp_diagnosis();
-    // do_shuffle_ys_iteration();
+    do_shuffle_ys_iteration();
     do_shuffle_ys_iteration_10();
     return 0;
 }
