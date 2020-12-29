c1ca5e5214a9e8c5e70f9775e602cb9dd98bc947
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-12-29 21:59:16 +0900

    change Travis-CI setting

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -ev
-docker exec -ti ${MY_NAME} bash -c "apt update -q" > /dev/null
-docker exec -ti ${MY_NAME} bash -c "apt upgrade -q -y" > /dev/null
-docker exec -ti ${MY_NAME} bash -c "DEBIAN_FRONTEND=noninteractive apt install -q -y build-essential git cmake libgmp-dev" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "apt-get update -q" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "apt-get upgrade -q -y" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "apt-get install -q -y build-essential git cmake libgmp-dev" > /dev/null
 
-docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/fmtlib/fmt.git -b 6.1.2 --depth 1 ${MY_TARGET_MOUNT}/third_party/fmt"
+docker exec -ti ${MY_NAME} bash -c "apt-get install -q -y libfmt-dev"
+#docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/fmtlib/fmt.git -b 6.1.2 --depth 1 ${MY_TARGET_MOUNT}/third_party/fmt"
 #docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
 
-docker exec -ti ${MY_NAME} bash -c "apt install -q -y libspdlog-dev libgtest-dev"
+docker exec -ti ${MY_NAME} bash -c "apt-get install -q -y libspdlog-dev libgtest-dev"
 
 #docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/google/googletest.git --depth 1"
 #docker exec -ti ${MY_NAME} bash -c "cd googletest; mkdir -p build; cd build; cmake .. -DBUILD_SHARED_LIBS=ON -DINSTALL_GTEST=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr; make -j; make install; ldconfig"
