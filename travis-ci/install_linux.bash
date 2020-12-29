85ef51f42ef7a1cc2bd086f8d9e81512ab51b8c4
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-12-29 22:50:36 +0900

    update

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -4,9 +4,10 @@ docker exec -ti ${MY_NAME} bash -c "apt-get update -q" > /dev/null
 docker exec -ti ${MY_NAME} bash -c "apt-get upgrade -q -y" > /dev/null
 docker exec -ti ${MY_NAME} bash -c "env DEBIAN_FRONTEND=noninteractive apt-get install -q -y build-essential git cmake libgmp-dev" > /dev/null
 
-docker exec -ti ${MY_NAME} bash -c "env DEBIAN_FRONTEND=noninteractive apt-get install -q -y libfmt-dev" > /dev/null
-#docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/fmtlib/fmt.git -b 6.1.2 --depth 1 ${MY_TARGET_MOUNT}/third_party/fmt"
-#docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
+#docker exec -ti ${MY_NAME} bash -c "env DEBIAN_FRONTEND=noninteractive apt-get install -q -y libfmt-dev" > /dev/null
+docker exec -ti ${MY_NAME} bash -c "git clone https://github.com/fmtlib/fmt.git --depth 1"
+# ${MY_TARGET_MOUNT}/third_party/fmt"
+docker exec -ti ${MY_NAME} bash -c "cd fmt; mkdir -p build; cd build; cmake ..; make -j; make install"
 
 docker exec -ti ${MY_NAME} bash -c "env DEBIAN_FRONTEND=noninteractive apt-get install -q -y libspdlog-dev libgtest-dev" > /dev/null
 
