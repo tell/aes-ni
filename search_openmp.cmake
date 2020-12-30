a523d76b2c2ab90720c002420a951719ef67df97
Tadanori TERUYA <tadanori.teruya@gmail.com>
2020-12-31 05:02:53 +0900

    update

--- search_openmp.cmake
+++ search_openmp.cmake
@@ -2,7 +2,7 @@ if(CMAKE_HOST_APPLE)
   # NOTE: See https://gitlab.kitware.com/cmake/cmake/-/blob/master/Modules/FindOpenMP.cmake
   message("Lookup OpenMP in macOS platform...")
   foreach(LANG IN ITEMS C CXX)
-    list(APPEND CMAKE_${LANG}_IMPLICIT_LINK_DIRECTORIES "/opt/local/lib/libomp")
+    list(APPEND CMAKE_${LANG}_IMPLICIT_LINK_DIRECTORIES "/usr/local/lib" "/opt/local/lib/libomp")
     message("CMAKE_${LANG}_IMPLICIT_LINK_DIRECTORIES = ${CMAKE_${LANG}_IMPLICIT_LINK_DIRECTORIES}")
     if(IS_DIRECTORY "/opt/local/include/libomp")
       set(OpenMP_${LANG}_INCLUDE_DIR "/opt/local/include/libomp")
