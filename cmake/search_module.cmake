a2b64700ae51eae77b4fb2edbe856b06f1353912
Tadanori TERUYA <tadanori.teruya@gmail.com>
2021-01-02 18:38:13 +0900

    update

--- cmake/search_module.cmake
+++ cmake/search_module.cmake
@@ -1,32 +1,13 @@
 include(FindPackageHandleStandardArgs)
 
-include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/search_gmp.cmake")
+include("${CMAKE_SOURCE_DIR}/cmake/search_gmp.cmake")
 
 find_package(Threads REQUIRED)
 
-include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/search_openmp.cmake")
-
-if(NOT (IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/third_party"))
-  message(WARNING "Exec mkdir ${CMAKE_CURRENT_SOURCE_DIR}/third_party")
-  make_directory("${CMAKE_CURRENT_SOURCE_DIR}/third_party")
-endif()
+include("${CMAKE_SOURCE_DIR}/cmake/search_openmp.cmake")
 
 find_package(fmt REQUIRED
-  HINTS "${CMAKE_CURRENT_SOURCE_DIR}/third_party/module/lib/cmake")
+  HINTS "${CMAKE_SOURCE_DIR}/third_party/module/lib/cmake")
 
 find_package(spdlog REQUIRED
-  HINTS "${CMAKE_CURRENT_SOURCE_DIR}/third_party/module/lib/cmake")
-
-#if(NOT (IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/third_party/randen"))
-#  make_directory("${CMAKE_CURRENT_SOURCE_DIR}/third_party/randen")
-#endif()
-#foreach(filename IN ITEMS "LICENSE" "vector128.h" "randen.h" "randen.cc")
-#  if(NOT (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/third_party/randen/${filename}"))
-#    message("Download: ${filename}")
-#    file(DOWNLOAD "https://raw.githubusercontent.com/google/randen/master/${filename}"
-#      "${CMAKE_CURRENT_SOURCE_DIR}/third_party/randen/${filename}"
-#      SHOW_PROGRESS TLS_VERIFY on)
-#  endif()
-#endforeach()
-add_library(randen "${CMAKE_CURRENT_SOURCE_DIR}/third_party/randen/randen.cc")
-target_include_directories(randen PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/third_party/randen")
+  HINTS "${CMAKE_SOURCE_DIR}/third_party/module/lib/cmake")
