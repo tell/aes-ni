302fadc8fc40004d1ef8c9c52352b621d74a5a15
Tadanori TERUYA <tadanori.teruya@gmail.com>
2021-07-15 19:16:55 +0900

    update

--- src/benchmark2.cpp
+++ src/benchmark2.cpp
@@ -5,10 +5,12 @@ using namespace std;
 namespace clt::bench {
 Experiment::vec_duration_t Experiment::run()
 {
+    constexpr size_t ratio = 10;
     using limit_time_t = decay_t<decltype(limit_time_)>;
 
     vec_duration_t results;
     const auto start_time = timer_t::now();
+    duration_t cum_etime = {};
     for (size_t i = 0; i < num_setup_; i++) {
         (void)setup_func_();
         for (size_t j = 0; j < num_reptition_; j++) {
@@ -17,11 +19,16 @@ Experiment::vec_duration_t Experiment::run()
             const auto end_time = timer_t::now();
             assert(check_func_());
 
-            results.emplace_back(
-                duration_cast<duration_t>(end_time - begin_time));
-            const auto current_time = timer_t::now();
-            if (limit_time_ <
-                duration_cast<limit_time_t>(current_time - start_time)) {
+            {
+                const auto elapsed_time =
+                    duration_cast<duration_t>(end_time - begin_time);
+                results.emplace_back(elapsed_time);
+                cum_etime += elapsed_time;
+            }
+            const auto total_elapsed_time = timer_t::now() - start_time;
+            if ((limit_time_ < duration_cast<limit_time_t>(cum_etime)) ||
+                ((ratio * limit_time_) <
+                 duration_cast<limit_time_t>(total_elapsed_time))) {
                 return results;
             }
         }
