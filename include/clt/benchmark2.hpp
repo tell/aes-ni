f904b982d40efec364ef1e0f5627387f07a2271b
Tadanori TERUYA <tadanori.teruya@gmail.com>
2021-08-16 00:57:58 +0900

    workaround

--- include/clt/benchmark2.hpp
+++ include/clt/benchmark2.hpp
@@ -9,7 +9,7 @@
 #include <boost/version.hpp>
 #include <boost/range/adaptor/transformed.hpp>
 #include <boost/range/algorithm/copy.hpp>
-#if BOOST_VERSION >= 107200
+#if 0 // BOOST_VERSION >= 107200
 #include <boost/math/statistics/univariate_statistics.hpp>
 #else
 #include <boost/math/tools/univariate_statistics.hpp>
@@ -20,6 +20,36 @@
 #include "util.hpp"
 
 namespace clt::bench {
+
+#ifdef CLT_BENCHMARK2_OLD_BOOST
+CLT_message("Workaround patch for old boost.");
+
+namespace workaround {
+
+template <class T> inline auto mean_and_sample_variance(const T &v)
+{
+    return std::make_tuple(boost::math::tools::mean(v),
+                           boost::math::tools::sample_variance(v));
+};
+
+template <class T> inline auto interquartile_range(T &v)
+{
+    using boost::math::tools::median;
+
+    std::sort(v.begin(), v.end());
+    const size_t n = v.size();
+    const ssize_t n_half = n / 2;
+    auto it_lb = v.begin();
+    auto it_le = std::next(it_lb, n_half);
+    auto it_ue = v.end();
+    auto it_ub = std::next(it_ue, -n_half);
+    const auto lm = median(it_lb, it_le);
+    const auto um = median(it_ub, it_ue);
+    return um - lm;
+};
+
+} // namespace workaround
+#endif
 
 template <class T> inline auto calc_stats(const T &data)
 {
@@ -35,13 +65,10 @@ template <class T> inline auto calc_stats(const T &data)
     using boost::math::statistics::interquartile_range;
 #else
     CLT_message("Workaround patch for old boost.");
-    const auto mean_and_sample_variance = [](auto &v) {
-        return std::make_tuple(boost::math::tools::mean(v),
-                               boost::math::tools::sample_variance(v));
-    };
+    using workaround::mean_and_sample_variance;
     using boost::math::tools::median;
     using boost::math::tools::median_absolute_deviation;
-    const auto interquartile_range = [](auto) -> double { return 0; };
+    using workaround::interquartile_range;
 #endif
 
     constexpr double nscale = ratio_t::num;
